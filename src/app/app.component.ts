8e3e49600d691bb3c61951c8add0855fb3b9b567
laurenz <laurenz.dilba@hotmail.de>
2024-09-23 22:38:14 +0200

    added history fixed chat input layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ChatComponent } from './chat/chat.component';
+import {HistoryComponent} from "./history/history.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ChatComponent],
+  imports: [RouterOutlet, ChatComponent, HistoryComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.sass',
 })
