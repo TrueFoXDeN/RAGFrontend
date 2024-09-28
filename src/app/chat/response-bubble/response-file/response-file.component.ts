d4b709e78303612f05be9d4951aa29b1b1ef39d5
laurenz <laurenz.dilba@hotmail.de>
2024-09-28 11:43:43 +0200

    added dialog to show text

--- src/app/chat/response-bubble/response-file/response-file.component.ts
+++ src/app/chat/response-bubble/response-file/response-file.component.ts
@@ -1,9 +1,10 @@
 import { Component, Input } from '@angular/core';
+import { DialogModule } from 'primeng/dialog';
 
 @Component({
   selector: 'app-response-file',
   standalone: true,
-  imports: [],
+  imports: [DialogModule],
   templateUrl: './response-file.component.html',
   styleUrl: './response-file.component.sass',
 })
@@ -11,4 +12,10 @@ export class ResponseFileComponent {
   @Input() file: string = '';
   @Input() page: number = 0;
   @Input() text: string = '';
+
+  textVisible: boolean = false;
+
+  showText() {
+    this.textVisible = !this.textVisible;
+  }
 }
